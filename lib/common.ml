86dfae8de8c4148fc4b9c5bc14b6f022e7741dd4
Eric Kalkman <eric.kalkman@pm.me>
2023-12-03 13:37:21 -0500

    Updated some common functions, including all and any to use seqs instead of lists

--- lib/common.ml
+++ lib/common.ml
@@ -17,12 +17,31 @@ let read_input_file fname =
 
 let char_to_str c = String.make 1 c
 
-let rec any p lst =
+(*let rec any p lst =
     match lst with
     | [] -> false
-    | x :: xs -> if p x then true else any p xs
+    | x :: xs -> if p x then true else any p xs*)
 
-let rec all p lst =
+let rec any p sq =
+    match Seq.uncons sq with
+    | None -> false
+    | Some (x, xs) -> if p x then true else any p xs
+
+(*let rec all p lst =
     match lst with
     | [] -> true
-    | x :: xs -> if p x then all p xs else false
+    | x :: xs -> if p x then all p xs else false*)
+let rec all p sq =
+    match Seq.uncons sq with
+    | None -> true
+    | Some (x, xs) -> if p x then all p xs else false
+
+let compose f g x = f (g x)
+
+let in_range x a b = x >= a && x <= b
+
+let make_range lo hi =
+    let rec aux acc hi = if hi < lo then acc else aux (Seq.cons hi acc) (hi - 1) in
+    aux Seq.empty hi
+
+let id x = x
