789dcefcf6a8bda6d5f1d3dd1f5584fabaa8cd9a
Eric Kalkman <eric.kalkman@pm.me>
2023-12-26 23:12:37 -0500

    Some comments and renaming

--- lib/day19.ml
+++ lib/day19.ml
@@ -1,6 +1,8 @@
 open Common
 open Parsing
 
+(* parser productions; these are just the tokenizers *)
+(* the actual logic starts at line 101 lol *)
 let node_parser = expect_lower |> at_least_one |> group >=> stringify_top
 let result_parser = node_parser >=>? expect_char 'A' >=>? expect_char 'R'
 let expr_parser = node_parser >=> expect_set "<>" >=> expect_int
@@ -14,21 +16,26 @@ let workflow_parser =
 let workflows_parser = expect_list workflow_parser skip_nl |> group
 let term_parser = node_parser >=> skip_char '=' >=> expect_int |> group
 
-let setting_parser =
+let binding_parser =
   skip_char '{'
   >=> expect_list term_parser (expect_char ',')
   >=> skip_char '}' |> group
 
-let settings_parser = expect_list setting_parser skip_nl |> group
-let input_parser = workflows_parser >=> skip_nl >=> skip_nl >=> settings_parser
+let bindings_parser = expect_list binding_parser skip_nl |> group
+let input_parser = workflows_parser >=> skip_nl >=> skip_nl >=> bindings_parser
 
+(* types to represent the workflows *)
 type node = string
 type decision = Accept | Reject
 type op = LT | GT
+(* result can be an accept/reject decision or another workflow (node) *)
 type result = Node of node | Decision of decision
-type cond = { op : op; node : node; cnst : int; res : result }
+(* conditional phrase: reg `op` cnst -> res; reg = "register", i.e., x, m, a, or s *)
+type cond = { op : op; reg : node; cnst : int; res : result }
+(* a workflow has a final "else" branch as well as a series of conditional phrases *)
 type workflow = { final : result; conds : cond list }
 
+(* glorious functional parsing *)
 let op_of_char = function
   | '<' -> LT
   | '>' -> GT
@@ -43,22 +50,24 @@ let process_result = function
 let process_cond cond =
   match cond |> unwrap_group with
   | [ PString name; Char c; Int x; res ] ->
-      { op = op_of_char c; node = name; cnst = x; res = process_result res }
+      { op = op_of_char c; reg = name; cnst = x; res = process_result res }
   | _ -> invalid_arg "process_cond"
 
 let process_workflow g =
   match g |> unwrap_group with
   | PString name :: rest -> (
+      (* the last item in rest is the target of the last else branch *)
       match List.rev rest with
-      | res :: conds ->
+      | final :: conds ->
           ( name,
             {
-              final = process_result res;
+              final = process_result final;
               conds = List.(rev conds |> map process_cond);
             } )
       | _ -> invalid_arg "failed to match workflow format: final")
   | _ -> invalid_arg "failed to match workflow format: head"
 
+(* settings for the XMAS system (second half of input) *)
 type binding = { x : int; m : int; a : int; s : int }
 
 let process_binding g =
@@ -72,13 +81,13 @@ let process_binding g =
       { x; m; a; s }
   | _ -> invalid_arg "process_binding"
 
-let index_binding x binding =
-  match x with
+let index_binding reg binding =
+  match reg with
   | "x" -> binding.x
   | "m" -> binding.m
   | "a" -> binding.a
   | "s" -> binding.s
-  | _ -> invalid_arg ("invalid XMAS code " ^ x)
+  | _ -> invalid_arg ("invalid XMAS code " ^ reg)
 
 module M = Map.Make (String)
 
@@ -89,20 +98,27 @@ let process_input inp =
         List.map process_binding bindings )
   | _ -> invalid_arg "failed to match two components"
 
-let eval_cond binds cond =
+let eval_cond bind cond =
+  (* evaluates a conditional clause given the XMAS settings in bind *)
   match cond.op with
-  | LT -> index_binding cond.node binds < cond.cnst
-  | GT -> index_binding cond.node binds > cond.cnst
+  | LT -> index_binding cond.reg bind < cond.cnst
+  | GT -> index_binding cond.reg bind > cond.cnst
 
 let rec eval_at wfs bind wf_name =
+  (* why do all the parsing above? so that this function can be stupidly
+     concise *)
   let wf = M.find wf_name wfs in
   match
     wf.conds |> List.to_seq
+    (* find the first successful conditional *)
     |> Seq.drop_while (eval_cond bind >> not)
     |> Seq.uncons
+    (* if there was no successful condition, return wf.final; else, return
+       the branch destination of the conditinoal (cond.res) *)
     |> Option.fold ~none:wf.final ~some:(fun (cond, _) -> cond.res)
   with
   | Decision x -> x
+  (* workflow goes to another workflow; recurse *)
   | Node name -> eval_at wfs bind name
 
 let part_1 inp =
@@ -111,43 +127,64 @@ let part_1 inp =
   |> List.filter (fun bind -> eval_at wfs bind "in" == Accept)
   |> List.fold_left (fun acc bind -> acc + bind.x + bind.m + bind.a + bind.s) 0
 
-let func_of_cond cond =
+let func_of_cond cond x =
   match cond.op with
-  | LT -> fun x -> x < cond.cnst
-  | GT -> fun x -> x > cond.cnst
+  | LT -> x < cond.cnst
+  | GT -> x > cond.cnst
 
 let count_paths wfs =
+  (* decidedly less concise than part 1 *)
+  (* builds One Giant Lambda to filter on the register ranges (1 .. 4000) while
+     traversing the workflow tree from its root. Each edge from a parent to a
+     child represents traversing the conditional. As such, a lambda that filters
+     according to the conditional is inductively &&'d onto the filter function for
+     the appropriate XMAS register at each step. When a leaf is reached, the filter
+     functions are applied over the sequence of ints 1 .. 4000, the final counts of
+     which are returned back up the tree. *)
   let rec count_paths' filterfuncs res =
     match res with
     | Decision Reject -> 0
     | Decision Accept ->
+        (* we've built the filter function, now apply it *)
         let max_rng = 4000 in
         let inp_rng = Seq.ints 1 |> Seq.take max_rng in
         let counter l =
           inp_rng |> Seq.filter (M.find l filterfuncs) |> Seq.length
         in
+        (* each register is independent, so the number of options stack
+           multiplicatively *)
         counter "x" * counter "m" * counter "a" * counter "s"
     | Node name ->
+        (* we've been sent to another workflow *)
         let wf = M.find name wfs in
         let counts, elsefuncs =
+          (* for each conditional of the current workflow *)
           wf.conds
           |> List.fold_left
                (fun (count, filters) cond ->
+                 (* function that screens register values according to the
+                    conditional described by cond *)
                  let condfunc = func_of_cond cond in
                  let cur_filterfuncs =
-                   update_unsafe M.update cond.node
-                     (fun conjnot x -> conjnot x && condfunc x)
+                   update_unsafe M.update cond.reg
+                   (* all preceding conditionals must succeed, as must the current one *)
+                     (fun filterfunc x -> filterfunc x && condfunc x)
                      filters
                  in
                  let cur_counts = count_paths' cur_filterfuncs cond.res in
+                 (* now consider the branch where the conditional failed. this branch
+                    filters on not (condfunc x) *)
                  let next_filterfuncs =
-                   update_unsafe M.update cond.node
-                     (fun conjnot x -> conjnot x && not (condfunc x))
+                   update_unsafe M.update cond.reg
+                     (fun filterfunc x -> filterfunc x && not (condfunc x))
                      filters
                  in
+                 (* counts scale additively here because we are partitioning the count
+                    among the edges of our tree *)
                  (count + cur_counts, next_filterfuncs))
                (0, filterfuncs)
         in
+        (* consider the case where all of the conditionals are false *)
         let elsecount = count_paths' elsefuncs wf.final in
         counts + elsecount
   in
