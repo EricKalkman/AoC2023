c19de3e0426a2719f6dddb2f70138ab89c26c0d1
Eric Kalkman <eric.kalkman@pm.me>
2023-12-28 01:47:12 -0500

    replaced filter with the proper set operation

--- lib/day22.ml
+++ lib/day22.ml
@@ -135,7 +135,6 @@ let num_supporting resting_on supporting b =
 let part_2 inp =
   let bricks = process_input inp |> stack in
   let resting_on, supporting = bricks |> gen_graph in
-  bricks |> PQ.to_seq
-  |> Seq.filter (fun b -> compare_brick b ground != 0)
+  bricks |> PQ.remove ground |> PQ.to_seq
   |> Seq.map (num_supporting resting_on supporting)
   |> sum
