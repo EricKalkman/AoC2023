2bd87fda599c80eebb35506141a4998224514c97
Eric Kalkman <eric.kalkman@pm.me>
2024-01-07 21:59:10 -0500

    Better power of 2 (bitshift) + formatting

--- lib/day04.ml
+++ lib/day04.ml
@@ -16,35 +16,33 @@ type game = { id : int; winning_nos : int list; player_nos : int list }
 let process_input lines =
   lines |> tokenize_input
   |> Seq.map (fun rawline ->
-         let line = unwrap_result rawline in
-         {
-           id = List.hd line |> unwrap_int;
-           winning_nos = List.nth line 1 |> unwrap_group |> List.map unwrap_int;
-           player_nos = List.nth line 2 |> unwrap_group |> List.map unwrap_int;
-         })
+         match rawline |> unwrap_result with
+         | [ Int id; Group wins; Group plays ] ->
+             {
+               id;
+               winning_nos = wins |> List.map unwrap_int;
+               player_nos = plays |> List.map unwrap_int;
+             }
+         | _ -> invalid_arg "invalid format")
 
 module IntS = Set.Make (Int)
 
 let n_wins g =
   let score_nums =
-    IntS.inter
-      (List.to_seq g.winning_nos |> IntS.of_seq)
-      (List.to_seq g.player_nos |> IntS.of_seq)
+    IntS.inter (g.winning_nos |> IntS.of_list) (g.player_nos |> IntS.of_list)
   in
   IntS.cardinal score_nums
 
 let score_game g =
   let wins = n_wins g in
-  if wins == 0 then 0
-  else make_range 1 wins |> Seq.drop 1 |> Seq.fold_left (fun acc _ -> 2 * acc) 1
+  if wins == 0 then 0 else Int.shift_left 1 (wins - 1)
 
-let part_1 lines =
-  lines |> process_input |> Seq.map score_game |> Seq.fold_left ( + ) 0
+let part_1 lines = lines |> process_input |> Seq.map score_game |> sum
 
 module IntM = CustomMap (Map.Make (Int))
 
 let part_2 lines =
-  let games = process_input lines in
+  let games = process_input lines |> Seq.memoize in
   let init_map = games |> Seq.map (fun g -> (g.id, 1)) |> IntM.of_seq in
   games
   |> Seq.fold_left
@@ -56,4 +54,4 @@ let part_2 lines =
            m
            (make_range (g.id + 1) (g.id + wins)))
        init_map
-  |> IntM.to_seq |> Seq.map snd |> Seq.fold_left ( + ) 0
+  |> IntM.to_seq |> Seq.map snd |> sum
